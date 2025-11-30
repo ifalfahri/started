7db64ff3f5f4c99067aed2295f8b289a6156f68f
Ifal Fahri A <ifalfahri16@gmail.com>
2025-11-30 09:50:24 +0800

    feat: implement home page with animations and floating elements

---
+++ lib/data.ts
@@ -0,0 +1,10 @@
+export const heroImages = [
+  { id: 1, url: "https://picsum.photos/seed/img27/400/400" },
+  { id: 2, url: "https://picsum.photos/seed/img17/400/400" },
+  { id: 3, url: "https://picsum.photos/seed/img13/400/520" },
+  { id: 4, url: "https://picsum.photos/seed/img117/400/400" },
+  { id: 5, url: "https://picsum.photos/seed/img20/400/400" },
+  { id: 6, url: "https://picsum.photos/seed/img6/520/400" },
+  { id: 7, url: "https://picsum.photos/seed/img21/400/400" },
+  { id: 8, url: "https://picsum.photos/seed/img9/400/480" },
+];
